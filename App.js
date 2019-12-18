26d3a0165fcb9a2ff4db172b02c9b582d47e2ab3
blackdanamite <seagrendaniel562@gmail.com>
2019-12-17 21:16:08 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Button from '../react-native-calculator/Components/Button/Button'
+
+export default class App extends Component {
+  render() {
+    let smileyFace ={ 
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/2000px-Mr._Smiley_Face.svg.png'
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <Text>My Name is Dan</Text>
+        <Image source={smileyFace} style={{width: 250, height: 500}}/>
+        <Text>I'm trying to learn React Native</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fa2',
+    alignItems: 'flex-start',
+    justifyContent: 'space-evenly',
+  },
+});
