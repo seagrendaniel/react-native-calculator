0ef8140ce8c8575c4ab8f460a2c759addd60ba67
blackdanamite <seagrendaniel562@gmail.com>
2019-12-20 12:51:25 -0800

    prop practice

--- App.js
+++ App.js
@@ -2,28 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from '../react-native-calculator/Components/Button/Button'
 
-export default class App extends Component {
+class Greeting extends Component {
   render() {
-    let smileyFace = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/2000px-Mr._Smiley_Face.svg.png'
-    }
-
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <Text>My Name is {this.props.name}</Text>
-        <Image source={smileyFace} style={{ width: 250, height: 500 }} />
-        <Text>I'm trying to learn React Native</Text>
+      <View style={{ alignItems: 'center' }}>
+        <Text> Hello my name is {this.props.name}</Text>
       </View>
     );
   }
 }
 
-export default class Person extends Component {
+export default class LotsOfGreetings extends Component {
   render() {
     return (
-      <View>
-        
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name={'Dan'}/>
+        <Greeting name={'Talia'}/>
+        <Greeting name={'Remi'}/>
       </View>
     )
   }
