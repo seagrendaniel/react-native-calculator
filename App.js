2199afd1af27dd4e813fbe0066a1a16fc3bd9563
blackdanamite <seagrendaniel562@gmail.com>
2019-12-20 13:30:00 -0800

    update styling

--- App.js
+++ App.js
@@ -2,33 +2,50 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from '../react-native-calculator/Components/Button/Button'
 
-class Greeting extends Component {
+class BlinkGreeting extends Component {
+
+  componentDidMount() {
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  state = { isShowingText: true };
+
   render() {
-    return (
-      <View style={{ alignItems: 'center' }}>
-        <Text> Hello my name is {this.props.name}</Text>
-      </View>
-    );
+    if (!this.state.isShowingText) {
+      return null
+    }
+
+      return (
+        <View>
+          <Text style={styles.bigBlue}> Hello my name is {this.props.name}</Text>
+        </View>
+      );
+    
   }
 }
 
 export default class LotsOfGreetings extends Component {
   render() {
     return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name={'Dan'}/>
-        <Greeting name={'Talia'}/>
-        <Greeting name={'Remi'}/>
+      <View>
+        <BlinkGreeting name={'Dan'} />
+        <BlinkGreeting name={'Talia'} />
+        <BlinkGreeting name={'Remi'} />
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fa2',
-    alignItems: 'flex-start',
-    justifyContent: 'space-evenly',
-  },
+  bigBlue: {
+    top: 150,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'blue',
+    alignItems: 'center'
+  }
 });
