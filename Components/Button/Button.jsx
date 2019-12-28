7562c2dec8b9a28071b5e600175bc0ba6c0799c5
blackdanamite <seagrendaniel562@gmail.com>
2019-12-28 09:27:26 -0800

    button change

--- Components/Button/Button.jsx
+++ Components/Button/Button.jsx
@@ -4,14 +4,32 @@ import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 class AlertButton extends Component {
   render(){
     return(
+      <View style={styles.container}>
       <Button
+        style={styles.orangeButton}
         onPress={() => {
           alert('You pressed me so hard!');
         }}
-        title='Press me!'
+        title='Press here'
+        color='purple'
       />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    top: 75,
+  },
+  orangeButton: {
+    margin: 20,
+    backgroundColor: 'blue',
+    borderWidth: 1,
+    borderColor: '#fff'
+  }
+  
+})
 
 export default AlertButton;
