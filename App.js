d33b096fc722e678effae9dcbc279b4d29b29ed3
blackdanamite <seagrendaniel562@gmail.com>
2020-01-13 12:12:28 -0800

    continue refactor

--- App.js
+++ App.js
@@ -2,102 +2,101 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import AlertButton from '../react-native-calculator/Components/Button/Button'
 
-class BlinkGreeting extends Component {
+// class BlinkGreeting extends Component {
 
-  componentDidMount() {
-    setInterval(() => (
-      this.setState(previousState => (
-        { isShowingText: !previousState.isShowingText }
-      ))
-    ), 1000);
-  }
+//   componentDidMount() {
+//     setInterval(() => (
+//       this.setState(previousState => (
+//         { isShowingText: !previousState.isShowingText }
+//       ))
+//     ), 1000);
+//   }
 
-  state = { isShowingText: true };
+//   state = { isShowingText: true };
 
-  render() {
-    if (!this.state.isShowingText) {
-      return null
-    }
+//   render() {
+//     if (!this.state.isShowingText) {
+//       return null
+//     }
 
-      return (
-        <View>
-          <Text style={{top: 75, textAlign: 'center'}}> Hello my name is {this.props.name}</Text>
-        </View>
-      );
+//       return (
+//         <View>
+//           <Text style={{top: 75, textAlign: 'center'}}> Hello my name is {this.props.name}</Text>
+//         </View>
+//       );
     
-  }
-}
+//   }
+// }
 
-class NumberInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {number : ''}
-  }
-  render(){
-    return(
-      <View>
-        <Button
-          title='1'
-          style=styles.
-        />
+// class NumberInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {number : ''}
+//   }
+//   render(){
+//     return(
+//       <View>
+//         <Button
+//           title='1'
+//         />
 
-      </View>
-    )
-  }
-}
+//       </View>
+//     )
+//   }
+// }
 
-class NameInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text : ''}
-  }
+// class NameInput extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {text : ''}
+//   }
 
-  render(){
-    return(
-      <View style={{top: 50, left: 50, textAlign: 'center'}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder='Enter name here!'
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-      </View>
-    )
-  }
-}
-
+//   render(){
+//     return(
+//       <View style={{top: 50, left: 50, textAlign: 'center'}}>
+//         <TextInput
+//           style={{height: 40}}
+//           placeholder='Enter name here!'
+//           onChangeText={(text) => this.setState({text})}
+//           value={this.state.text}
+//         />
+//       </View>
+//     )
+//   }
+// }
 
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{flex: 1, backgroundColor: 'lightblue'}}>
-          <Text style={{top: 50, left: 50}}>Hi my name is </Text>
-          <NameInput/>
-          <AlertButton/>
-        <BlinkGreeting name={'Dan'} />
-        </View>
-        <View style={{flex: 1, backgroundColor: 'lightgreen'}}>
-          <Text style={{top: 50, left: 50}}>Hi my name is </Text>
-          <NameInput/>
-          <AlertButton/>
-        {/* <BlinkGreeting name={'Talia'} /> */}
-        </View>
-        <View style={{flex: 1, backgroundColor: 'pink'}}>
-          <Text style={{top: 50, left: 50}}>Hi my name is </Text>
-          <NameInput/>
-          <AlertButton/>
-        {/* <BlinkGreeting name={'Remi'} /> */}
-        </View>
-      </View>
-    )
-  }
-}
+
+// export default class LotsOfGreetings extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={{flex: 1, backgroundColor: 'lightblue'}}>
+//           <Text style={{top: 50, left: 50}}>Hi my name is </Text>
+//           <NameInput/>
+//           <AlertButton/>
+//         <BlinkGreeting name={'Dan'} />
+//         </View>
+//         <View style={{flex: 1, backgroundColor: 'lightgreen'}}>
+//           <Text style={{top: 50, left: 50}}>Hi my name is </Text>
+//           <NameInput/>
+//           <AlertButton/>
+//         {/* <BlinkGreeting name={'Talia'} /> */}
+//         </View>
+//         <View style={{flex: 1, backgroundColor: 'pink'}}>
+//           <Text style={{top: 50, left: 50}}>Hi my name is </Text>
+//           <NameInput/>
+//           <AlertButton/>
+//         {/* <BlinkGreeting name={'Remi'} /> */}
+//         </View>
+//       </View>
+//     )
+//   }
+// }
 
 // --- Calculator Layout --- //
 
-class Calculator extends Component {
+export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -106,9 +105,12 @@ class Calculator extends Component {
   }
   render() {
     return(
-      <View>
+      <View style={{flex: 1}}>
+        <View style={styles.ioDisplay}>
+
+        </View>
         <View>
-          
+
         </View>
       </View>
     )
@@ -133,5 +135,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'blue',
     fontWeight: 'bold'
+  },
+  ioDisplay: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  calcButtonContainer: {
+    flex: 5,
+    backgroundColor: 'red'
   }
 });
