28228a1629be2f11cf42683f9325bc23cfcd2260
blackdanamite <seagrendaniel562@gmail.com>
2020-01-13 16:11:41 -0800

    for fork

--- App.js
+++ App.js
@@ -1,6 +1,196 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import AlertButton from '../react-native-calculator/Components/Button/Button'
+import AddButton from '../react-native-calculator/Components/CalcButtons/AddButton/AddButton';
+import ClearButton from '../react-native-calculator/Components/CalcButtons/ClearButton/ClearButton';
+import DivButton from '../react-native-calculator/Components/CalcButtons/DivButton/DivButton';
+import EqButton from '../react-native-calculator/Components/CalcButtons/EqButton/EqButton';
+import MultButton from '../react-native-calculator/Components/CalcButtons/MultButton/MultButton';
+import NumberButton from '../react-native-calculator/Components/CalcButtons/NumberButton/NumberButton';
+import PercentButton from '../react-native-calculator/Components/CalcButtons/PercentButton/PercentButton';
+import PolarButton from '../react-native-calculator/Components/CalcButtons/PolarButton/PolarButton';
+import SubButton from '../react-native-calculator/Components/CalcButtons/SubButton/SubButton';
+
+
+// --- Calculator Layout --- //
+
+export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentValue: 0,
+      operatorValue: null,
+      storedValue: 0
+    }
+  }
+  render() {
+    return(
+      <View style={{flex: 1}}>
+
+        <View style={styles.ioDisplay}>
+          <Text style={styles.ioText}>{this.state.currentValue}</Text>
+        </View>
+
+        <View style={styles.calcButtonContainer}>
+          {/* Row 1 */}
+        <View style={styles.calcRow1}>
+        <View style={styles.calcButton1}>
+          <ClearButton />
+        </View>
+        <View style={styles.calcButton2}>
+          <PolarButton />
+        </View>
+        <View style={styles.calcButton1}>
+          <PercentButton />
+        </View>
+        <View style={styles.calcButton2}>
+          <DivButton />
+        </View>
+        </View>
+        {/* Row 2 */}
+        <View style={styles.calcRow2}>
+        <View style={styles.calcButton1}>
+          <Text style={styles.calcText}> 7 </Text>
+          <NumberButton/>
+        </View>
+        <View style={styles.calcButton2}>
+          <Text style={styles.calcText}> 8 </Text>
+        </View>
+        <View style={styles.calcButton1}>
+          <Text style={styles.calcText}> 9 </Text>
+        </View>
+        <View style={styles.calcButton2}>
+          <MultButton />
+        </View>
+        </View>
+        {/* Row 3 */}
+        <View style={styles.calcRow1}>
+        <View style={styles.calcButton1}>
+          <Text style={styles.calcText}> 4 </Text>
+        </View>
+        <View style={styles.calcButton2}>
+        <Text style={styles.calcText}> 5 </Text>
+        </View>
+        <View style={styles.calcButton1}>
+        <Text style={styles.calcText}> 6 </Text>
+        </View>
+        <View style={styles.calcButton2}>
+        <SubButton />
+        </View>
+        </View>
+        {/* Row 4 */}
+        <View style={styles.calcRow2}>
+        <View style={styles.calcButton1}>
+        <Text style={styles.calcText}> 1 </Text>
+        </View>
+        <View style={styles.calcButton2}>
+        <Text style={styles.calcText}> 2 </Text>
+        </View>
+        <View style={styles.calcButton1}>
+        <Text style={styles.calcText}> 3 </Text>
+        </View>
+        <View style={styles.calcButton2}>
+        <AddButton />
+        </View>
+        </View>
+        {/* Row 5 */}
+        <View style={styles.calcRow1}>
+        <View style={styles.calcButton3}>
+        <Text style={styles.calcText}> 0 </Text>
+        </View>
+        <View style={styles.calcButton1}>
+        <Text style={styles.calcText}> . </Text>
+        </View>
+        <View style={styles.calcButton2}>
+        <EqButton />
+        </View>
+        </View>
+
+      </View>
+      </View>
+    )
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    right: 500,
+    top: 150,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'blue',
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1
+  },
+  numberButton: {
+    fontSize: 20,
+    color: 'blue',
+    fontWeight: 'bold'
+  },
+  ioDisplay: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'flex-end',
+    borderBottomWidth: 2.5
+  },
+  calcButtonContainer: {
+    flex: 5,
+    backgroundColor: 'black',
+  },
+  calcRow1: {
+    flex: 1,
+    color: 'lightblue',
+    flexDirection: 'row'
+  },
+  calcRow2: {
+    flex: 1,
+    color: 'purple',
+    flexDirection: 'row'
+  },
+  calcButton1: {
+    flex: 1,
+    backgroundColor: 'green',
+    borderRadius: 3,
+    borderWidth: 2.5,
+    borderColor: 'black',
+    justifyContent: 'center'
+  },
+  calcButton2: {
+    flex: 1,
+    backgroundColor: 'orange',
+    borderRadius: 3,
+    borderWidth: 2.5,
+    borderColor: 'black',
+    justifyContent: 'center'
+  },
+  calcButton3: {
+    flex: 2,
+    backgroundColor: 'lightblue',
+    borderRadius: 3,
+    borderWidth: 2.5,
+    borderColor: 'black',
+    justifyContent: 'center'
+  },
+  ioText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    textAlignVertical: 'bottom'
+  },
+  calcText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  }
+
+});
+
+
+// --- Reference Code (to be deleted) ---//
 
 // class BlinkGreeting extends Component {
 
@@ -93,177 +283,3 @@ import AlertButton from '../react-native-calculator/Components/Button/Button'
 //     )
 //   }
 // }
-
-// --- Calculator Layout --- //
-
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentValue: 0,
-    }
-  }
-  render() {
-    return(
-      <View style={{flex: 1}}>
-
-        <View style={styles.ioDisplay}>
-          <Text style={styles.ioText}>{this.state.currentValue}</Text>
-        </View>
-
-        <View style={styles.calcButtonContainer}>
-          {/* Row 1 */}
-        <View style={styles.calcRow1}>
-        <View style={styles.calcButton1}>
-          <Text style={styles.calcText}>A/C</Text>
-        </View>
-        <View style={styles.calcButton2}>
-          <Text style={styles.calcText}> +/- </Text>
-        </View>
-        <View style={styles.calcButton1}>
-          <Text style={styles.calcText}> % </Text>
-        </View>
-        <View style={styles.calcButton2}>
-          <Text style={styles.calcText}> / </Text>
-        </View>
-        </View>
-        {/* Row 2 */}
-        <View style={styles.calcRow2}>
-        <View style={styles.calcButton1}>
-          <Text style={styles.calcText}> 7 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-          <Text style={styles.calcText}> 8 </Text>
-        </View>
-        <View style={styles.calcButton1}>
-          <Text style={styles.calcText}> 9 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-          <Text style={styles.calcText}> x </Text>
-        </View>
-        </View>
-        {/* Row 3 */}
-        <View style={styles.calcRow1}>
-        <View style={styles.calcButton1}>
-          <Text style={styles.calcText}> 4 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-        <Text style={styles.calcText}> 5 </Text>
-        </View>
-        <View style={styles.calcButton1}>
-        <Text style={styles.calcText}> 6 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-        <Text style={styles.calcText}> - </Text>
-        </View>
-        </View>
-        {/* Row 4 */}
-        <View style={styles.calcRow2}>
-        <View style={styles.calcButton1}>
-        <Text style={styles.calcText}> 1 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-        <Text style={styles.calcText}> 2 </Text>
-        </View>
-        <View style={styles.calcButton1}>
-        <Text style={styles.calcText}> 3 </Text>
-        </View>
-        <View style={styles.calcButton2}>
-        <Text style={styles.calcText}> + </Text>
-        </View>
-        </View>
-        {/* Row 5 */}
-        <View style={styles.calcRow1}>
-        <View style={styles.calcButton3}>
-        <Text style={styles.calcText}> 0 </Text>
-        </View>
-        <View style={styles.calcButton1}>
-        <Text style={styles.calcText}> . </Text>
-        </View>
-        <View style={styles.calcButton2}>
-        <Text style={styles.calcText}> = </Text>
-        </View>
-        </View>
-
-      </View>
-      </View>
-    )
-  }
-}
-
-
-
-const styles = StyleSheet.create({
-  bigBlue: {
-    right: 500,
-    top: 150,
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'blue',
-    alignItems: 'center'
-  },
-  container: {
-    flex: 1
-  },
-  numberButton: {
-    fontSize: 20,
-    color: 'blue',
-    fontWeight: 'bold'
-  },
-  ioDisplay: {
-    flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'flex-end'
-  },
-  calcButtonContainer: {
-    flex: 5,
-    backgroundColor: 'black',
-  },
-  calcRow1: {
-    flex: 1,
-    color: 'lightblue',
-    flexDirection: 'row'
-  },
-  calcRow2: {
-    flex: 1,
-    color: 'purple',
-    flexDirection: 'row'
-  },
-  calcButton1: {
-    flex: 1,
-    backgroundColor: 'green',
-    borderRadius: 3,
-    borderWidth: 2.5,
-    borderColor: 'black',
-    justifyContent: 'center'
-  },
-  calcButton2: {
-    flex: 1,
-    backgroundColor: 'orange',
-    borderRadius: 3,
-    borderWidth: 2.5,
-    borderColor: 'black',
-    justifyContent: 'center'
-  },
-  calcButton3: {
-    flex: 2,
-    backgroundColor: 'lightblue',
-    borderRadius: 3,
-    borderWidth: 2.5,
-    borderColor: 'black',
-    justifyContent: 'center'
-  },
-  ioText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'right',
-    textAlignVertical: 'bottom'
-  },
-  calcText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    textAlignVertical: 'center'
-  }
-
-});
