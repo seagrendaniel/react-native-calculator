458939be488ac89e396a844646a154e4e0a3dbb5
blackdanamite <seagrendaniel562@gmail.com>
2020-01-14 18:47:07 -0800

    add decimal btn

---
+++ Components/CalcButtons/DecButton/DecButton.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import {Button} from 'react-native-elements';
+
+class DecButton extends Component {
+  render() {
+    return(
+      <View>
+        <Button
+          style={styles.calcText}
+          onPress={() => {
+            alert('You pressed the Decimal button');
+          }}
+          title='.'
+          titleStyle={{
+            color: 'black',
+            fontSize: 40,
+            fontWeight: 'bold'
+          }}
+          type='clear'
+        >
+        </Button>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  calcText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'black'
+  }
+})
+
+export default DecButton;
